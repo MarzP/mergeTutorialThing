cdc686532b5b0832c2ef5fe90feb16b255d2b203
TheMightyHoosier <ec2-user@ip-172-31-34-238.us-east-2.compute.internal>
2019-12-05 21:35:38 +0000

    Appocalypse

--- app.js
+++ app.js
@@ -3,15 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.use(session({
+  secret: 'ude5xiQSPl',
+  // resave: false,
+  // saveUninitialized: true,
+  // cookie: {secure: true}
+}));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +29,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+//////////////////////////////////////////////////
+
+var lab9Router = require('./public/Labs/Lab9/router');
+app.use('/Labs/Lab9', lab9Router);
+
+var lab10Router = require('./public/Labs/Lab10/router');
+app.use('/Labs/Lab10', lab10Router);
+
+//////////////////////////////////////////////////
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
